7a625ea7412032b38f74d99237e798f2d547009d
puzzlebros <ale.barredo@gmail.com>
2025-07-15 11:21:38 -0300

    update

--- public/data/faq.ts
+++ public/data/faq.ts
@@ -12,7 +12,7 @@ export const faqs: Faq[] = [
       "It is a game and NFT collection. Anyone can play it and collect it.",
   },
   {
-    question: "How are they related?",
+    question: "How are they game and the NFT related?",
     answer:
       "The game has a ranking. Every weekend, the top 10 can claim a free NFT from the collection.",
   },
@@ -41,9 +41,14 @@ export const faqs: Faq[] = [
     answer:
       "To mint means that you become the rightful owner of an item of the collection. You need a tiny bit of sol to cover for the network cost associated to that transaction that makes that possible.",
   },
-    {
+  {
     question: "What else can I know about the collection?",
     answer:
       "The collection is a Metaplex Core NFT Standard, which ensures world class security and efficiency recognized by the biggest marketplaces such as Magic Eden and Tensor.",
+  },
+  {
+    question: "How is my wallet used in this app?",
+    answer:
+      "Your wallet is not used by any commercial purpose and your account data is not collected ever by this app. Your connected wallet is perfectly safe and private, and it only serves the purpose of recording your score in the ranking to mint from the Metaplex Core collection. Ranking data is cleared every week and no wallet data is ever stored anywhere.",
   },
 ];
