8e778e1b51fc2d40213c1152369a9dcf31e93b7a
puzzlebros <ale.barredo@gmail.com>
2026-02-17 18:53:13 -0300

    compression test 2

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,33 +7,6 @@ export default function Document() {
       <Head>
         {/* your other fonts, meta tags, etc. */}
         <link rel="stylesheet" href="https://use.typekit.net/wul4mrm.css" />
-
-        {/* ——— Unity WebGL Preloads (raw only) ——— */}
-      <link rel="preload" href="/UnityBuild/Build/Jumper.loader.js" as="script" />
-
-      <link
-        rel="preload"
-        href="/UnityBuild/Build/Jumper.framework.js.br"
-        as="fetch"
-        crossOrigin="anonymous"
-      />
-
-      <link
-        rel="preload"
-        href="/UnityBuild/Build/Jumper.wasm.br"
-        as="fetch"
-        type="application/wasm"
-        crossOrigin="anonymous"
-      />
-
-      <link
-        rel="preload"
-        href="/UnityBuild/Build/Jumper.data.br"
-        as="fetch"
-        type="application/octet-stream"
-        crossOrigin="anonymous"
-      />
-        {/* ————————————— end Unity Preloads ————————————— */}
       </Head>
       <body>
         <Main />
